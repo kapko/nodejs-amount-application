916d48afa7d05e5be6dea0b5ea72cc878ad70dbd
kzhekshen <kzhekshen@incentient.com>
2018-12-12 23:09:50 +0600

    mongod fixed

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // connection
-mongoose.connect('mongodb://localhost:27017/amount');
+mongoose.connect(
+    'mongodb://kapar:123123Kk@ds151060.mlab.com:51060/amount',
+);
+
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -17,28 +20,6 @@ db.once('open', () => {
 
 app.use('/api', require('./auth/authorization.router'));
 app.use('/api/posts', require('./routers/post.router'));
-
-// app.get('/api/posts', auth.verifyToken, (req, res) => {
-//     auth.verify(req.token)
-//         .then(token => {
-//             res.send('ok');
-//         })
-//         .catch(err => res.sendStatus(err));
-// });
-
-// app.post('/api/login', (req, res) => {
-//     const name = req.body.name;
-//     const password = req.body.password;
-
-//     .findOne({name, password}, (err , user) => {
-//         if (!user) return res.sendStatus(401);
-
-//         auth.getToken(user)
-//             .then(token => res.send(token))
-//             .catch(err => res.send(err));
-
-//     });
-// });
 
 app.listen(4000, function () {
     console.log('Example app listening on port 3000!');
